4bf1464f9b2f1ed448f06787cf16bc4de1b762ca
ThiaggoDev <sfrancoraigosa@gmail.com>
2024-04-15 17:49:03 -0500

    El proyecto ha sido finalizado

---
+++ public/js/js.js
@@ -0,0 +1,371 @@
+
+
+
+//class Hotwheels{
+//    constructor(name, id, img, button){
+//            this.img = img;
+//            this.id = id;
+//            this.name = name;
+//            this.button = button;
+//    }
+//}
+
+//const mustangImageURL = URL="./img/20230818_133046.jpg";
+//const mustang = new Hotwheels("Mustang", 1, mustangImageURL, "button");
+
+//const toyotaImageURL = URL="./img/20230818_133116.jpg";
+//const toyota = new Hotwheels("toyota", 2, toyotaImageURL, "button");
+
+let button5 = document.getElementById("button5")
+button5.addEventListener("click", toyota)
+let toyota1 = document.getElementById("toyota") 
+toyota1.style.display = "none"
+
+let button6 = document.getElementById("button6")
+button6.addEventListener("click", mustang)
+let mustang1 = document.getElementById("mustang")
+mustang1.style.display = "none"
+
+let button7 = document.getElementById("button7")
+button7.addEventListener("click", primero1)
+let Primero  = document.getElementById("primero")
+Primero.style.display = "none"
+
+let button8 = document.getElementById("button8")
+button8.addEventListener("click", f1)
+let f11  = document.getElementById("f1")
+f11.style.display = "none"
+
+let button9 = document.getElementById("button9")
+button9.addEventListener("click", doblecara)
+let doble  = document.getElementById("doblecara")
+doble.style.display = "none"
+
+let button10 = document.getElementById("button10")
+button10.addEventListener("click", turbo)
+let turbo1 = document.getElementById("turbo1")
+turbo1.style.display = "none"
+
+let button11 = document.getElementById("button11")
+button11.addEventListener("click", numero)
+let numero1 = document.getElementById("numero")
+numero1.style.display = "none"
+
+let button12 = document.getElementById("button12")
+button12.addEventListener("click", numero80)
+let numero81 = document.getElementById("numero80")
+numero81.style.display = "none"
+
+let button13 = document.getElementById("button13")
+button13.addEventListener("click", vaca1)
+let vaca = document.getElementById("vaca")
+vaca.style.display = "none"
+
+let button14 = document.getElementById("button14")
+button14.addEventListener("click", mula2)
+let mula1 = document.getElementById("mula")
+mula1.style.display = "none"
+
+let button15 = document.getElementById("button15")
+button15.addEventListener("click", moto1)
+let moto = document.getElementById("moto")
+moto.style.display = "none"
+
+let button16 = document.getElementById("button16")
+button16.addEventListener("click", croc1)
+let croc = document.getElementById("croc")
+croc.style.display = "none"
+
+let button17 = document.getElementById("button17")
+button17.addEventListener("click", arg1)
+let arg = document.getElementById("arg")
+arg.style.display = "none"
+
+function toyota(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = ""
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function mustang(){
+    mustang1.style.display = ""
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function primero1(){
+    mustang1.style.display = "none"
+    primero.style.display = ""
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function f1(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = ""
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function doblecara(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = ""
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function turbo(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = ""
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function numero(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = ""
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function numero80(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = ""
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function vaca1(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = ""
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function mula2(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = ""
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function moto1(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = ""
+    croc.style.display = "none"
+    arg.style.display = "none"
+}
+
+function croc1(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = ""
+    arg.style.display = "none"
+}
+
+function arg1(){
+    mustang1.style.display = "none"
+    primero.style.display = "none"
+    f11.style.display = "none"
+    doble.style.display = "none"
+    toyota1.style.display = "none"
+    turbo1.style.display = "none"
+    numero1.style.display = "none"
+    numero81.style.display = "none"
+    vaca.style.display = "none"
+    mula1.style.display = "none"
+    moto.style.display = "none"
+    croc.style.display = "none"
+    arg.style.display = ""
+}
+
+
+let barra1 = document.getElementById("barra_busqueda")
+let button1 = document.getElementById("button1")
+button1.addEventListener("click", modoOscuro)
+button1.style.backgroundColor = "white"
+button1.style.color = "#333"
+
+let section1 = document.getElementById("section1")
+section1.style.backgroundColor = "white"
+let ul3 = document.getElementById("ul_3")
+ul3.style.backgroundColor = "#edeaea"
+let butto = document.getElementById("butto")
+butto.style.backgroundColor = "white"
+butto.style.color = "#333"
+let logo = document.getElementById("logo")
+logo.style.backgroundColor = "white"
+let ul4 = document.getElementById("ul_4")
+let enlace = document.getElementById("enlace_resultados")
+
+let button2 = document.getElementById("button2")
+button2.addEventListener("click", modoClaro)
+button2.style.display = "none"
+
+let main = document.getElementById("main")
+
+function modoOscuro(){
+    main.style.backgroundColor = "#444444"
+    button2.style.display = ""
+    button1.style.display = "none"
+    section1.style.backgroundColor = "#333"
+    button2.style.backgroundColor = "#333"
+    button2.style.color = "antiquewhite"
+    butto.style.backgroundColor = "#333"
+    butto.style.color = "antiquewhite"
+    logo.style.backgroundColor = "#333"
+    ul3.style.backgroundColor = "#3b3b3b"
+    ul4.style.backgroundColor = "#3b3b3b"
+    barra1.style.color = "antiquewhite"
+    enlace.style.backgroundColor = "#333"
+    enlace.style.border = ""
+    enlace.style.color = "#e8e8e8"
+}
+
+function modoClaro(){
+    main.style.backgroundColor = "white"
+    button1.style.display = ""
+    button2.style.display = "none"
+    section1.style.backgroundColor = "white"
+    button1.style.backgroundColor = "white"
+    button1.style.color = "#333"
+    butto.style.backgroundColor = "white"
+    butto.style.color = "#333"
+    logo.style.backgroundColor = "white"
+    ul3.style.backgroundColor = "#edeaea"
+    ul4.style.backgroundColor = "#edeaea"
+    barra1.style.color = "#333"
+    enlace.style.backgroundColor = "#e8e8e8"
+    enlace.style.border = "none"
+    enlace.style.color = "#333"
+}
+
+    // Captura el clic en el enlace de búsqueda.
+    var enlaceResultados = document.getElementById("enlace_resultados");
+    enlaceResultados.addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el enlace navegue a la página directamente
+    
+    // Obtén el valor de búsqueda
+    var busqueda = document.getElementById("barra_busqueda").value;
+    
+    // Redirige a la página de resultados con el término de búsqueda en la URL
+    window.location.href = "./js/resultados.html?query=" + encodeURIComponent(busqueda);
+    });
+
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+  
+    loader.style.display = "none";
+    content.style.display = "block";
+});
